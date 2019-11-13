31a88771b2d059021c7d19963eb9e29b6fa0deb3
AuntiStaci <Staciluu@gmail.com>
2019-11-13 18:33:53 +0000

    added the final parts finishing touches

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 // middleware
 app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.json());
 let list = ["Code and watch anime", "Slackline tonight"];
 
 // ROUTES
@@ -25,6 +25,14 @@ app.post("/ninja", function(req, res) {
   console.log(req.body.taskItem);
   list.push(req.body.taskItem);
   res.render("home.ejs", { list: list });
+});
+
+app.delete("/delete/:index", function(req, res) {
+  console.log(req.params.index);
+
+  list.splice(req.params.index, 1);
+
+  res.json(list);
 });
 
 // server listeing for request
